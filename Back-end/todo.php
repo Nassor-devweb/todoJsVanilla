690e97bedb9b3e68ecc737663a67210309c7622a
HIMIDI Ahmed <nassor.devweb@gmail.com>
2023-07-11 21:29:39 +0200

    insert tache in db

--- Back-end/todo.php
+++ Back-end/todo.php
@@ -1,14 +1,32 @@
 <?php
 
+require_once("./classConnexionDb.php");
 require_once('./authQuerry.php');
 
+$pdo = Connexion::connectDb();
 $isConnect = Auth::authUser();
 
 if ($isConnect) {
-    $message = [];
-    $message['message'] = "Authentification réussi";
-    $res = json_encode($message);
-    echo $res;
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $finished_tache = 0;
+        $tacheJson = file_get_contents('php://input');
+        $tache = json_decode($tacheJson, true);
+        $date = date(DateTime::ATOM, time());
+        $stmt = $pdo->prepare('INSERT INTO tache VALUES (
+            DEFAULT,
+            :nom_tache,
+            :finished_tache,
+            :date_created,
+            :id_user
+        )');
+
+        $stmt->bindValue(':nom_tache', $tache['tache']);
+        $stmt->bindValue(':finished_tache', $finished_tache);
+        $stmt->bindValue(':date_created', $date);
+        $stmt->bindValue(':id_user', $isConnect['id_user']);
+
+        $stmt->execute();
+    }
 } else {
     header('Content-Type : application/json');
     http_response_code(401);
